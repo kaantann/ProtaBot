4ba28af3eb6e447f80ace7b173cd26e5a58b1b10
Kaan Tan <kaantan97@gmail.com>
2023-03-21 07:06:34 +0300

    downgrade has been applied, poll signature has been changed

--- GeneralCommands.cs
+++ GeneralCommands.cs
@@ -6,7 +6,6 @@ using DSharpPlus.Interactivity.Extensions;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace ProtaBot_v1._0
@@ -52,74 +51,80 @@ namespace ProtaBot_v1._0
             await ctx.Channel.SendMessageAsync(embed);
         }
         [Command("poll")]
-        [Cooldown(2, 200, CooldownBucketType.User)]
-        public async Task PollCommand(CommandContext ctx, string Option1, string Option2, string Option3, string Option4,int timeLimitSec ,params string[] Question)
+        public async Task PollCommand(CommandContext ctx, int timeLimitSec,string Option1, string Option2, string Option3, string Option4 ,params string[] Question)
         {
-            var interactivity = ctx.Client.GetInteractivity();
-            TimeSpan timer = TimeSpan.FromSeconds(timeLimitSec);
+            var interactvity = ctx.Client.GetInteractivity(); 
+            TimeSpan timer = TimeSpan.FromSeconds(timeLimitSec); 
 
-            DiscordEmoji[] optionEmojis =
-            {
-                DiscordEmoji.FromName(ctx.Client, ":one:",false ),
-                DiscordEmoji.FromName(ctx.Client, ":two:",false ),
-                DiscordEmoji.FromName(ctx.Client, ":three:",false ),
-                DiscordEmoji.FromName(ctx.Client, ":four:",false )
-            };
+            DiscordEmoji[] optionEmojis = { DiscordEmoji.FromName(ctx.Client, ":one:", false),
+                                            DiscordEmoji.FromName(ctx.Client, ":two:", false),
+                                            DiscordEmoji.FromName(ctx.Client, ":three:", false),
+                                            DiscordEmoji.FromName(ctx.Client, ":four:", false) }; 
 
             string optionsString = optionEmojis[0] + " | " + Option1 + "\n" +
-                                                   optionEmojis[1] + " | " + Option2 + "\n" +
-                                                   optionEmojis[2] + " | " + Option3 + "\n" +
-                                                   optionEmojis[3] + " | " + Option4;
+                                   optionEmojis[1] + " | " + Option2 + "\n" +
+                                   optionEmojis[2] + " | " + Option3 + "\n" +
+                                   optionEmojis[3] + " | " + Option4; 
 
-            var pollMessage = new DiscordMessageBuilder().AddEmbed(new DiscordEmbedBuilder()
+            var pollMessage = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
 
                 .WithColor(DiscordColor.Azure)
                 .WithTitle(string.Join(" ", Question))
                 .WithDescription(optionsString)
-                );
+                ); 
 
-            var putReaction = await ctx.Channel.SendMessageAsync(pollMessage);
+            var putReactOn = await ctx.Channel.SendMessageAsync(pollMessage); 
 
             foreach (var emoji in optionEmojis)
             {
-                await putReaction.CreateReactionAsync(emoji);
+                await putReactOn.CreateReactionAsync(emoji); 
             }
 
-            var result = await interactivity.CollectReactionsAsync(putReaction, timer);
+            var result = await interactvity.CollectReactionsAsync(putReactOn, timer); 
 
-            int count1 = 0;
+            int count1 = 0; 
             int count2 = 0;
             int count3 = 0;
             int count4 = 0;
 
-            foreach (var emoji in result)
+            foreach (var emoji in result) 
             {
                 if (emoji.Emoji == optionEmojis[0])
+                {
                     count1++;
+                }
                 if (emoji.Emoji == optionEmojis[1])
+                {
                     count2++;
+                }
                 if (emoji.Emoji == optionEmojis[2])
+                {
                     count3++;
+                }
                 if (emoji.Emoji == optionEmojis[3])
+                {
                     count4++;
+                }
             }
 
             int totalVotes = count1 + count2 + count3 + count4;
 
-            string resultString = optionEmojis[0] + ": " + count1 + " Votes \n" +
-                optionEmojis[1] + ": " + count2 + " Votes \n" +
-                optionEmojis[2] + ": " + count3 + " Votes \n" +
-                optionEmojis[3] + ": " + count4 + " Votes \n\n" +
-                "The total number of votes is : " + totalVotes;
+            string resultsString = optionEmojis[0] + ": " + count1 + " Votes \n" +
+                       optionEmojis[1] + ": " + count2 + " Votes \n" +
+                       optionEmojis[2] + ": " + count3 + " Votes \n" +
+                       optionEmojis[3] + ": " + count4 + " Votes \n\n" +
+                       "The total number of votes is " + totalVotes; 
 
-            var resultsMessage = new DiscordMessageBuilder().AddEmbed(new DiscordEmbedBuilder()
+            var resultsMessage = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
 
                 .WithColor(DiscordColor.Green)
-                .WithTitle("Results of the Poll")
-                .WithDescription(resultString)
+                .WithTitle("Results of Poll")
+                .WithDescription(resultsString)
                 );
 
-            await ctx.Channel.SendMessageAsync(resultsMessage);
+            await ctx.Channel.SendMessageAsync(resultsMessage); 
 
         }
 
